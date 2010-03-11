c75f4a01e1bce4fcb866d7f2ba38d1af4582bbad
Michael Stead <michael.stead@gmail.com>
2010-03-10 22:21:37 -0400

    GWT service now searching and displaying shows
    - now using ted-core's service to lookup shows.

--- ted-ui/gwt/GwtTed/src/nu/ted/ui/gwt/server/page/SearchServiceImpl.java
+++ ted-ui/gwt/GwtTed/src/nu/ted/ui/gwt/server/page/SearchServiceImpl.java
@@ -7,6 +7,7 @@ import org.apache.thrift.TException;
 
 import nu.ted.client.ClientAction;
 import nu.ted.client.JavaClient;
+import nu.ted.gen.SeriesSearchResult;
 import nu.ted.gen.WatchedSeries;
 import nu.ted.gen.TedService.Client;
 import nu.ted.ui.gwt.client.page.SearchService;
@@ -17,16 +18,16 @@ public class SearchServiceImpl extends RemoteServiceServlet implements
 		SearchService {
 
 	@Override
-	public List<String> search(String filter) {
+	public List<String> search(final String filter) {
 		JavaClient tedClient = new JavaClient("localhost", 9030);
 		final ArrayList<String> shows = new ArrayList<String>();
 		tedClient.run(new ClientAction() {
 			
 			@Override
 			public void run(Client client) throws TException {
-				List<WatchedSeries> watching = client.getWatching();
-				for (WatchedSeries w : watching) {
-					shows.add(w.getName());
+				List<SeriesSearchResult> found = client.search(filter);
+				for (SeriesSearchResult serie : found) {
+					shows.add("(" + serie.getUid() + ") " + serie.getName());
 				}
 			}
 		});
