8d5fecf50ce315dac9c12080848f63fd35505e12
Michael Stead <michael.stead@gmail.com>
2010-03-31 18:56:57 -0300

    click ted logo to go to the watch list.

--- ted-ui/gwt-ted/src/nu/ted/gwt/client/PageContainerLayout.java
+++ ted-ui/gwt-ted/src/nu/ted/gwt/client/PageContainerLayout.java
@@ -1,11 +1,14 @@
 package nu.ted.gwt.client;
 
+import net.bugsquat.gwtsite.client.PageLoader;
 import net.bugsquat.gwtsite.client.layout.DefaultPageContainerLayout;
 
 import nu.ted.gwt.client.i18n.TedMessages;
 import nu.ted.gwt.client.image.Images;
 import nu.ted.gwt.client.Css.Application;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
@@ -21,7 +24,15 @@ public class PageContainerLayout extends DefaultPageContainerLayout {
 	protected FlowPanel createHeader() {
 		FlowPanel header = new FlowPanel();
 		header.setStyleName(Application.TED_HEADER);
-		header.add(new Image(Images.INSTANCE.headerLogo()));
+		Image headerImage = new Image(Images.INSTANCE.headerLogo());
+		headerImage.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				PageLoader.getInstance().loadPage(TedPageId.WATCHED_SERIES);
+			}
+		});
+		header.add(headerImage);
 
 		Label title = new Label(TedMessages.INSTANCE.headerTitle());
 		title.setStyleName(Application.TED_HEADER_TITLE);
