b978941ec2c6704b9f361346b97785bedf0b9a40
Michael Stead <michael.stead@gmail.com>
2010-04-04 16:54:03 -0300

    Implemented add event to registry.

--- ted-server/src/nu/ted/event/EventRegistry.java
+++ ted-server/src/nu/ted/event/EventRegistry.java
@@ -4,52 +4,84 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+
+import nu.ted.generated.Event;
 
 public class EventRegistry {
 
 	protected static final int MAX_TRIES = 50;
-	
+
 	private ClientIdGenerator idGenerator;
-	private Map<String, List<String>> registry;
-	
+	private Map<String, List<Event>> registry;
+
 	public EventRegistry() {
 		this(new ClientIdGenerator());
 	}
-	
+
 	public EventRegistry(ClientIdGenerator clientIdGenerator) {
 		this.idGenerator = clientIdGenerator;
-		this.registry = new HashMap<String, List<String>>();
+		this.registry = new HashMap<String, List<Event >>();
 	}
-	
-	public String registerClient() {		
-		String generatedId = null;;
-		int count = 1;
-		
-		// Adding a max tries just to be certain that if the generator ever
-		// messed up, we don't get caught in an infinite loop.
-		//
-		// NOTE: THIS IS NOT LIKELY TO EVER HAPPEN (BETTER SAFE THAN SORRY).
-		while(count != MAX_TRIES) {
-			generatedId = this.idGenerator.generateClientId();
-			if (registry.containsKey(generatedId)) {
-				count++;
-				continue;
+
+	public String registerClient() {
+		synchronized(registry) {
+			String generatedId = null;;
+			int count = 1;
+
+			// Adding a max tries just to be certain that if the generator ever
+			// messed up, we don't get caught in an infinite loop.
+			//
+			// NOTE: THIS IS NOT LIKELY TO EVER HAPPEN (BETTER SAFE THAN SORRY).
+			while(count != MAX_TRIES) {
+				generatedId = this.idGenerator.generateClientId();
+				if (registry.containsKey(generatedId)) {
+					count++;
+					continue;
+				}
+				break;
 			}
-			break;
+
+			if (count == MAX_TRIES) {
+				// TODO [MS] Throw properly typed exception here?
+				throw new RuntimeException("Unable to generate unique client ID for Client.");
+			}
+
+			registry.put(generatedId, new ArrayList<Event>());
+			return generatedId;
 		}
-		
-		if (count == MAX_TRIES) {
-			// TODO [MS] Throw properly typed exception here?
-			throw new RuntimeException("Unable to generate unique client ID for Client.");
-		}
-		
-		registry.put(generatedId, new ArrayList<String>());
-		return generatedId;
 	}
 
 	public boolean isRegistered(String clientId) {
-		return registry.containsKey(clientId);
+		synchronized(registry) {
+			return registry.containsKey(clientId);
+		}
 	}
 
-	
+	public void addEvent(Event event) {
+		synchronized(registry) {
+			for (Entry<String, List<Event>> entry: registry.entrySet()) {
+				entry.getValue().add(event);
+			}
+		}
+	}
+
+	public List<Event> getEvents(String clientId) {
+		synchronized(registry) {
+			try {
+				if (!isRegistered(clientId)) {
+					// TODO [MS] Throw properly typed exception here?
+					throw new RuntimeException("Unable to get events. Client not registered: " +
+							clientId);
+				}
+
+				return registry.get(clientId);
+			}
+			finally {
+				registry.put(clientId, new ArrayList<Event>());
+			}
+		}
+	}
+
+
 }
--- ted-server/test/nu/ted/event/EventRegistryTest.java
+++ ted-server/test/nu/ted/event/EventRegistryTest.java
@@ -9,6 +9,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
+import nu.ted.generated.Event;
+import nu.ted.generated.EventType;
+
 import org.junit.Test;
 
 public class EventRegistryTest {
@@ -19,24 +22,24 @@ public class EventRegistryTest {
 
 		TestClientIdGenerator idGenerator = new TestClientIdGenerator();
 		EventRegistry registry = new EventRegistry(idGenerator);
-		
+
 		idGenerator.setAvailableIds(clientId);
 		String registryKey = registry.registerClient();
 		assertEquals(clientId, registryKey);
 	}
-	
+
 	@Test
 	public void registeringClientAddsClientIdToKeyList() {
 		String expectedClientId = "C1-GWT";
 
 		TestClientIdGenerator idGenerator = new TestClientIdGenerator();
 		EventRegistry registry = new EventRegistry(idGenerator);
-		
+
 		idGenerator.setAvailableIds(expectedClientId);
 		String clientId = registry.registerClient();
 		assertTrue("Expected registry to contain client key: " + clientId, registry.isRegistered(clientId));
 	}
-	
+
 	@Test
 	public void ensureUniqueIdIsReturnedOnRegisterClientIfGeneratorHappenedToReturnAnAlreadyRegisteredId() {
 		String expectedClientId1 = "C1-GWT";
@@ -44,7 +47,7 @@ public class EventRegistryTest {
 
 		TestClientIdGenerator idGenerator = new TestClientIdGenerator();
 		EventRegistry registry = new EventRegistry(idGenerator);
-		
+
 		// Add ID1 twice in a row to simulate that it gets generated again the 2nd time registerClient is called.
 		// NOTE that the test generator is implemented using a stack (first in, last out).
 		idGenerator.setAvailableIds(expectedClientId2, expectedClientId1, expectedClientId1);
@@ -52,33 +55,82 @@ public class EventRegistryTest {
 		assertEquals(expectedClientId2, registry.registerClient());
 		assertTrue(idGenerator.isEmpty());
 	}
-	
+
 	@Test(expected = RuntimeException.class)
 	public void ensureTimeoutFoeRegisterClientIfGeneratorNeverGeneratesAUniqueId() {
 		String clientId = "C1-GWT";
 
 		// Set the same ID one more than max_attempts so register will time out.
 		ArrayList<String> sameIds = new ArrayList<String>();
-		
+
 		// MAX_TRIES + 2: One ID for the initial registration of the ID, and
 		//                another to ensure we go over the maximum.
 		int max = EventRegistry.MAX_TRIES + 2;
 		for(int i = 0; i < max; i++) {
 			sameIds.add(clientId);
 		}
-		
+
 		TestClientIdGenerator idGenerator = new TestClientIdGenerator();
 		idGenerator.setAvailableIds(sameIds.toArray(new String[sameIds.size()]));
-		
+
 		EventRegistry registry = new EventRegistry(idGenerator);
-		
+
 		// Ok for the first attempt.
 		registry.registerClient();
-		
+
 		// Same ID will be tried until MAX_ATTEMPTS is reached and an exception is thrown.
 		registry.registerClient();
 	}
-	
+
+	@Test
+	public void checkEventAssociatedWithAllRegisteredClients() {
+		TestClientIdGenerator idGenerator = new TestClientIdGenerator();
+		EventRegistry registry = new EventRegistry(idGenerator);
+
+		idGenerator.setAvailableIds("CID_2", "CID_1");
+
+		String clientId1 = registry.registerClient();
+		String clientId2 = registry.registerClient();
+
+		Event watchedListChanged = new Event(EventType.WATCHED_LIST_CHANGED);
+		registry.addEvent(watchedListChanged);
+
+		List<Event> client1Events = registry.getEvents(clientId1);
+		assertEquals(1, client1Events.size());
+		assertEquals(watchedListChanged, client1Events.get(0));
+
+		List<Event>	client2Events = registry.getEvents(clientId2);
+		assertEquals(1, client2Events.size());
+		assertEquals(watchedListChanged, client2Events.get(0));
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void ensureExceptionIsThrownIfClientIsNotRegisteredOnGetEvents() {
+		TestClientIdGenerator generator = new TestClientIdGenerator();
+		EventRegistry registry = new EventRegistry(generator);
+		registry.getEvents("SS");
+	}
+
+	@Test
+	public void ensureEventListIsClearedAfterRetrieval() {
+		TestClientIdGenerator idGenerator = new TestClientIdGenerator();
+		EventRegistry registry = new EventRegistry(idGenerator);
+
+		idGenerator.setAvailableIds("CID_1");
+
+		String clientId1 = registry.registerClient();
+
+		Event watchedListChanged = new Event(EventType.WATCHED_LIST_CHANGED);
+		registry.addEvent(watchedListChanged);
+
+		List<Event> client1Events = registry.getEvents(clientId1);
+		assertEquals(1, client1Events.size());
+		assertEquals(watchedListChanged, client1Events.get(0));
+
+		assertTrue("Events should be empty after get.",
+				registry.getEvents(clientId1).isEmpty());
+	}
+
 	private class TestClientIdGenerator extends ClientIdGenerator {
 		private Stack<String> available = new Stack<String>();
 
@@ -89,16 +141,16 @@ public class EventRegistryTest {
 			}
 			return available.pop();
 		}
-		
+
 		public void setAvailableIds(String ... ids) {
 			available.clear();
 			available.addAll(Arrays.asList(ids));
 		}
-		
+
 		public boolean isEmpty() {
 			return available.isEmpty();
 		}
-		
+
 	}
 
 }
