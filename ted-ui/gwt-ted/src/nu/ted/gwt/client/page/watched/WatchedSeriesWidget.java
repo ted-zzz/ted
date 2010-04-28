44b3636624769e5317a8cc54130064dd0dcfae3e
Michael Stead <michael.stead@gmail.com>
2010-04-28 19:31:14 -0300

    Image changes.

--- ted-ui/gwt-ted/src/nu/ted/gwt/client/page/watched/WatchedSeriesWidget.java
+++ ted-ui/gwt-ted/src/nu/ted/gwt/client/page/watched/WatchedSeriesWidget.java
@@ -61,8 +61,23 @@ public class WatchedSeriesWidget extends Composite {
 		seriesContent.add(info);
 
 		FlowPanel watchingPanel = new FlowPanel();
+//		watchingPanel.setStyleName(Css.WatchedSeriesPage.WATCHED_SERIES);
+//		watchingPanel.add(actionPanel);
+//		watchingPanel.add(info);
+//		watchingPanel.add(imagePanel);
+
+		FlowPanel lhs = new FlowPanel();
+		lhs.setStyleName("ted-watched-series-LHS");
+
+		FlowPanel rhs = new FlowPanel();
+		rhs.setStyleName("ted-watched-series-RHS");
+		rhs.add(stopWatchingImage);
+
+		FlowPanel stuff = new FlowPanel();
+		stuff.setStyleName("ted-watched-series-CONTENT");
+
 		watchingPanel.add(rhs);
-		watchingPanel.add(seriesContent);
+		watchingPanel.add(stuff);
 		watchingPanel.add(imagePanel);
 		watchingPanel.add(lhs);
 		return watchingPanel;
