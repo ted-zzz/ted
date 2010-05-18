bb471168c053cbbfe0e99aca474cd9083be57b33
Kenny MacDermid <kenny.macdermid@gmail.com>
2010-05-18 10:28:06 -0400

    Fix one more getEvent that sneaked in from the merge.

--- ted-server/src/test/java/nu/ted/domain/SeriesBackendWrapperTest.java
+++ ted-server/src/test/java/nu/ted/domain/SeriesBackendWrapperTest.java
@@ -7,10 +7,8 @@ import nu.ted.Server;
 import nu.ted.generated.Event;
 import nu.ted.generated.EventType;
 import nu.ted.generated.Series;
-import nu.ted.generated.SeriesSearchResult;
 import nu.ted.guide.GuideFactory;
 import nu.ted.guide.TestGuide;
-import nu.ted.guide.tvdb.TVDB;
 import nu.ted.service.TedServiceImpl;
 
 import static org.junit.Assert.*;
@@ -23,13 +21,13 @@ public class SeriesBackendWrapperTest {
 		TestGuide seriesSource = new TestGuide();
 		GuideFactory.addGuide(seriesSource);
 		TedServiceImpl service = new TedServiceImpl(Server.createDefaultTed(), seriesSource);
-		String clientId = service.registerClientWithEventRegistry();
+		service.registerClientWithEventRegistry();
 
 		Series series = new Series();
 		series.setGuideName(seriesSource.getName());
 		SeriesBackendWrapper wrapper = new SeriesBackendWrapper(series);
 		wrapper.update(Calendar.getInstance());
-		List<Event> events = service.getEvents(clientId);
+		List<Event> events = service.getEvents();
 		assertEquals(1, events.size());
 
 		Event received = events.get(0);
