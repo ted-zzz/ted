efd9e852d91e4717d81d44dee8e3767667b3a0c0
Kenny MacDermid <kenny.macdermid@gmail.com>
2010-07-04 01:27:19 +0800

    Add Rome RSS Source.
    Only a manual test around this and also needs the exceptions handled
    correctly.

--- ted-server/src/main/java/nu/ted/torrent/search/Rss.java
+++ ted-server/src/main/java/nu/ted/torrent/search/Rss.java
@@ -1,45 +1,86 @@
 package nu.ted.torrent.search;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedInput;
+
+import nu.ted.guide.DataTransferException;
 import nu.ted.torrent.Torrent;
+import nu.ted.www.DirectPageLoader;
+import nu.ted.www.PageLoader;
 
 public class Rss {
-	
+
 	public static interface RssSource {
 		public List<Torrent> getTorrents();
 	}
-	
+
 	public static class RomeRssSource implements RssSource {
+
+		private String location;
+
+		public RomeRssSource(String location) {
+			this.location = location;
+		}
 
 		@Override
 		public List<Torrent> getTorrents() {
-			// TODO Auto-generated method stub
-			throw new UnsupportedOperationException("Not Yet Implemented");
+			List<Torrent> results = new LinkedList<Torrent>();
+
+			try {
+				PageLoader pageLoader = new DirectPageLoader();
+				InputStream inputStream = pageLoader.openStream(location);
+				SyndFeedInput feedInput = new SyndFeedInput();
+				SyndFeed feed = feedInput.build(new InputStreamReader(inputStream));
+
+				@SuppressWarnings("rawtypes")
+				Iterator iter = feed.getEntries().iterator();
+				while (iter.hasNext()) {
+					SyndEntry entry = (SyndEntry) iter.next();
+					results.add(new Torrent(entry.getTitle(), entry.getLink()));
+				}
+			} catch (DataTransferException e) {
+				// TODO: log error - probably ignore
+				throw new RuntimeException("Unhandled DTE", e);
+			} catch (IllegalArgumentException e) {
+				// TODO: log error - probably ignore
+				throw new RuntimeException("Unhandled IAE", e);
+			} catch (FeedException e) {
+				// TODO: log error - probably ignore
+				throw new RuntimeException("Unhandled FE", e);
+			}
+
+			return results;
 		}
 	}
-	
+
 	private RssSource source;
-	
+
 	public Rss(RssSource source) {
 		this.source = source;
 	}
-	
-	public Rss() {
-		this.source = new RomeRssSource();
+
+	public Rss(String location) {
+		this.source = new RomeRssSource(location);
 	}
 
 	public List<Torrent> search(String term) {
 		List<Torrent> torrents = source.getTorrents();
 		List<Torrent> results = new LinkedList<Torrent>();
-		
+
 		for (Torrent torrent : torrents) {
 			if (torrent.getTitle().contains(term)) {
 				results.add(torrent);
 			}
 		}
-		
+
 		return results;
 	}
 
--- ted-server/src/test/java/nu/ted/torrent/search/RssTest.java
+++ ted-server/src/test/java/nu/ted/torrent/search/RssTest.java
@@ -6,26 +6,27 @@ import java.util.LinkedList;
 import java.util.List;
 
 import nu.ted.torrent.Torrent;
+import nu.ted.torrent.search.Rss.RomeRssSource;
 import nu.ted.torrent.search.Rss.RssSource;
 
 import org.junit.Test;
 
 public class RssTest {
-	
+
 	private static class TestRssSource implements RssSource {
 
 		@Override
 		public List<Torrent> getTorrents() {
-			
+
 			List<Torrent> torrents = new LinkedList<Torrent>();
 			torrents.add(new Torrent("A-name1", "A-link1"));
 			torrents.add(new Torrent("A-name2", "A-link2"));
 			torrents.add(new Torrent("B-name", "B-link"));
 			torrents.add(new Torrent("C-name", "C-link"));
-			
+
 			return torrents;
 		}
-		
+
 	}
 
 	@Test
@@ -34,9 +35,17 @@ public class RssTest {
 		List<Torrent> torrents = rss.search("A-name");
 		assertNotNull(torrents);
 		assertEquals(2, torrents.size());
-		
+
 		assertEquals("A-name1" , torrents.get(0).getTitle());
 		assertEquals("A-name2" , torrents.get(1).getTitle());
+	}
+
+	// Set location and a breakpoint to test Rome's processing.
+	// Could have RomeRssSouce() take an input stream for testing.
+	public void manualRomeDebug() {
+		String location = "set location here";
+		RomeRssSource source = new RomeRssSource(location);
+		source.getTorrents();
 	}
 
 }
