740e5efad8aa144a3a29cbf5f1516e8b18e3ec4d
Kenny MacDermid <kenny.macdermid@gmail.com>
2010-10-15 06:33:28 +0800

    Re-fix python UI to use code.InteractiveConsole.
    Commands now need the () afterwards, but we can easily create new
    objects so they can be passed in commands as arguments.

--- ted-ui/py-ted/ted-ui.py
+++ ted-ui/py-ted/ted-ui.py
@@ -37,9 +37,7 @@ class TedUI():
 
         self.builtins = {}
         self.builtins['connect'] = self.connect
-        self.builtins['disconnect'] = self.disconnect
         self.builtins['help'] = self.help
-        self.builtins['?'] = self.help
 
     def tab_complete(self, text, index):
         matches = [word for word in self.funs if word.startswith(text)]
@@ -51,13 +49,13 @@ class TedUI():
     def _show_commands(self):
         print "Commands"
         print "--------"
-        for key in self.builtins:
+        keys = [k for k in self.builtins if not k[0] == '_']
+        keys.sort()
+        for key in keys:
             print key
         print "--------"
-        for fun in self.funs:
-            print fun
 
-    def connect(self):
+    def connect(self, *args):
         self.transport = TBufferedTransport(TSocket('localhost', 9030))
         self.transport.open()
         protocol = TBinaryProtocol.TBinaryProtocol(self.transport)
@@ -65,13 +63,24 @@ class TedUI():
         self.client = ted.TedService.Client(protocol)
         self.connected = True
 
-    def disconnect(self):
+        for fun in self.funs:
+            self.builtins[fun] = getattr(self.client, fun)
+
+        del self.builtins['connect']
+        self.builtins['disconnect'] = self.disconnect
+
+    def disconnect(self, *args):
+        for fun in self.funs:
+            del self.builtins[fun]
         self.client.logout()
         self.transport.close()
         self.client = None
         self.connected = False
 
-    def help(self):
+        del self.builtins['disconnect']
+        self.builtins['connect'] = self.connect
+
+    def help(self, *args):
         self._show_commands()
 
     def main(self):
@@ -79,49 +88,10 @@ class TedUI():
         logger = logging.getLogger('ted')
         logger.addHandler(logging.StreamHandler())
 
-        print "Welcome to ted. Type 'help' or '?' for a list of commands"
 
-        while 1:
-            try:
-                line = raw_input("> ")
-            except KeyboardInterrupt:
-                break
-            except EOFError:
-                break
+        shell = code.InteractiveConsole(self.builtins)
+        shell.interact("Welcome to ted. Type 'help()' for a list of commands")
 
-            if len(line) == 0:
-                continue
-
-            # Split lines using shlex, attempt to convert ints
-            # int's can be escaped using quotes
-            parser = shlex.shlex(line)
-            line_split = list(parser)
-            formatted_split = []
-            for item in line_split:
-                try:
-                    formatted_split.append(int(item))
-                except ValueError:
-                    if item[0] in parser.quotes:
-                        #Ignore Quotes
-                        formatted_split.append(item[1:-1])
-                    else:
-                        formatted_split.append(item)
-
-            command = formatted_split[0]
-            args = formatted_split[1:]
-            
-            if command in self.builtins:
-                self.builtins[command]()
-            elif command in self.funs:
-                if self.connected:
-                    c = getattr(self.client, command)
-                    print c(*args)
-                else:
-                    print "Unable to run while not connected"
-            else:
-                print "Invalid Command: " + command
-
-        print
         if self.connected:
             self.disconnect()
 
