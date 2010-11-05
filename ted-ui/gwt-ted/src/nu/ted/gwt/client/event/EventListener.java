c54f28da822a657fdede89b47497b60f1b8a602b
Michael Stead <michael.stead@gmail.com>
2010-11-05 08:31:50 -0300

    Removing GWT-TED as it has been replaced with PHP-TED.

--- ted-ui/gwt-ted/src/nu/ted/gwt/client/event/EventListener.java
+++
@@ -1,9 +0,0 @@
-package nu.ted.gwt.client.event;
-
-import nu.ted.gwt.domain.event.GwtEvent;
-
-public interface EventListener<E extends GwtEvent> {
-
-	void onEvent(E event);
-
-}
--- ted-ui/gwt-ted/src/nu/ted/gwt/client/widget/table/TedTableModelListener.java
+++
@@ -1,9 +0,0 @@
-package nu.ted.gwt.client.widget.table;
-
-public interface TedTableModelListener {
-
-	public void modelChanged();
-	
-	public void dataCleared();
-	
-}
