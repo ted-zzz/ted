978b71bbdf9b10869708293267a067392ea4c054
Michael Stead <michael.stead@gmail.com>
2010-11-23 21:28:52 -0400

    Fixed broken link to JQuery lib.
    - This broke polling.

--- ted-ui/php-ted/src/views/template.php
+++ ted-ui/php-ted/src/views/template.php
@@ -2,7 +2,7 @@
 	<head>
 		<title>TED - <?php echo $view_title; ?> </title>
 		<link href="/<?php echo __SITE_ROOT; ?>/views/ted.css" rel="stylesheet" type="text/css" />
-		<script src="lib/jquery.js" language="javascript" type="text/javascript"></script>
+		<script src="/<?php echo __SITE_ROOT; ?>/lib/jquery.js" language="javascript" type="text/javascript"></script>
 	</head>
 	<body>
 		<div id="container">
