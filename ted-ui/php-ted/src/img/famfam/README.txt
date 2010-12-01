d5a0271b44bd0c535e2ba7a416c5fa28fe939bd3
Michael Stead <michael.stead@gmail.com>
2010-12-01 09:05:48 -0400

    Added button images on watching page.

--- ted-ui/php-ted/src/img/famfam/README.txt
+++ ted-ui/php-ted/src/img/famfam/README.txt
@@ -1,3 +1,3 @@
-Icons under this folder are from the "Silk icon set 1.3"
+Icons under this folder are from the "Silk icon set 1.3" and Silk Companion 1
 
 http://www.famfamfam.com/lab/icons/silk/
--- ted-ui/php-ted/src/views/watching_content.php
+++ ted-ui/php-ted/src/views/watching_content.php
@@ -1,26 +1,38 @@
 
-<div id='<?php echo $series->uid; ?>'>
-	<h3><?php echo $series->name ?></h3>
-	<ul>
-		<?php
-			if (sizeof($series->episodes) == 0) {
-				echo "No episodes found.";
-			}
-			else {
-				foreach ($series->episodes as $episode) {
-					echo "<LI>Season " . $episode->season . " - Episode ". $episode->number . " [";
-					$date_string = $episode->aired->value;
-
-					// TODO This needs to be part of a date lib.
-					date_default_timezone_set('America/Halifax');
-					$date = new DateTime();
-					$date->setTimestamp($episode->aired->value / 1000);
-					echo $date->format('Y-m-d H:i:s');
-					echo "]";
+<table id='<?php echo $series->uid; ?>' class='watching-table'>
+	<tr>
+		<td class="watched-series-header"><h3><?php echo $series->name ?></h3></td>
+	</tr>
+	<tr>
+		<td class="watched-series-details">
+			<?php
+				if (sizeof($series->episodes) == 0) {
+					echo "No episodes found.";
 				}
+				else {
+					foreach ($series->episodes as $episode) {
+						echo "<LI>Season " . $episode->season . " - Episode ". $episode->number . " [";
+						$date_string = $episode->aired->value;
+	
+						// TODO This needs to be part of a date lib.
+						date_default_timezone_set('America/Halifax');
+						$date = new DateTime();
+						$date->setTimestamp($episode->aired->value / 1000);
+						echo $date->format('Y-m-d H:i:s');
+						echo "]";
+					}
 
-			}
-		?>
-	</ul>
-	<a href="watching/stopWatching/<?php echo $series->uid; ?>">Stop Watching</a>
-</div>
+				}
+			?>
+		</td>
+	<tr>
+	<tr>
+		<td>
+			<a href="watching/stopWatching/<?php echo $series->uid; ?>" class="button">
+				<img src="<?php echo '/' . __SITE_ROOT . '/img/famfam/television_delete.png'?>" alt="" />
+				Stop Watching
+			</a>
+		</td>
+	</tr>
+</table>
+
